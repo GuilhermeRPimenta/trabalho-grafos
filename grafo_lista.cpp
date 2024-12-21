5598a59ddd9600569a1a401c499d7a97fcb372ce
Rafael <rafaelgawry@gmail.com>
2024-12-21 15:09:07 -0300

    grafo_lista

---
+++ grafo_lista.cpp
@@ -0,0 +1,56 @@
+#include "grafo_lista.h"
+#include <string>
+#include <iostream>
+
+    grafo_lista::grafo_lista() 
+    {
+
+    }
+    grafo_lista::~grafo_lista() 
+    {
+
+    }
+    bool eh_bipartido()
+    {
+        return false;
+    }
+    int get_grau(int vertice)
+    {
+        return 0;
+    }
+    int get_ordem()
+    {
+        return 0;
+    }
+    bool eh_direcionado()
+    {
+        return false;
+    }
+    bool vertice_ponderado()
+    {
+        return false;
+    }
+    bool aresta_ponderada()
+    {
+        return false;
+    }
+    bool eh_completo()
+    {
+        return false;
+    }
+    bool eh_arvore()
+    {
+        return false;
+    }
+    bool possui_articulacao()
+    {
+        return false;
+    }
+    bool possui_ponte()
+    {
+        return false;
+    }
+    void carrega_grafo(const std::string& arquivo)
+    {
+
+    }
