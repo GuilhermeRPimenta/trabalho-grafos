584c175b185b7dfb2bc6ab55d9123eb3c795e4fb
Joao J <etiontdn@gmail.com>
2024-12-21 15:08:58 -0300

    inicialização de matriz

--- grafo_matriz.h
+++ grafo_matriz.h
@@ -1,9 +1,37 @@
 #ifndef GRAFO_MATRIZ_H_INCLUDED
 #define GRAFO_MATRIZ_H_INCLUDED
 #include "grafo.h"
+#include "iostream"
+
+using namespace std;
 
 class GrafoMatriz: public Grafo {
+    public:
+        int **matriz;
+        int *vertices;
+        GrafoMatriz() {
+            /*
+            Apenas para motivos de teste:
+            */
+           iniciar_matriz(5, true);
+        }
 
+        
+        void carrega_grafo() {
+
+        }
+    private:
+        void iniciar_matriz(int n_vertices, bool direcionado) {
+            matriz = new int*[n_vertices];
+            for (int i = 0; i < n_vertices; i++) {
+                matriz[i] = new int[n_vertices];
+                for (int j = 0; j < n_vertices; j++) {
+                    matriz[i][j] = 0;
+                }
+            }
+
+            vertices = new int[n_vertices];
+        }
 };
 
 #endif
